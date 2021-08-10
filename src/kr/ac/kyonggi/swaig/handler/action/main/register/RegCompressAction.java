277327c58d0565539f120c90ddd8a24a99af63aa
soyoung125 <sypark041754@gmail.com>
2021-08-11 03:42:33 +0900

    [박소영] 신청하기 답변 파일 압축하기 성공 / 다운로드는 안됨

---
+++ src/kr/ac/kyonggi/swaig/handler/action/main/register/RegCompressAction.java
@@ -0,0 +1,84 @@
+package kr.ac.kyonggi.swaig.handler.action.main.register;
+
+import kr.ac.kyonggi.swaig.common.controller.Action;
+import kr.ac.kyonggi.swaig.handler.dao.settings.RegisterDAO;
+import kr.ac.kyonggi.swaig.handler.dto.settings.RegisterDTO;
+import kr.ac.kyonggi.swaig.handler.zip.CompressZip;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+
+public class RegCompressAction implements Action {
+    @Override
+    public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        String boardId = request.getParameter("id");
+        String path = request.getServletContext().getRealPath("/uploaded/bbs_reg/reg"+boardId); // 압축할 파일이 저장된 위치
+        RegisterDAO dao = RegisterDAO.getInstance();
+        RegisterDTO checkReg = dao.getReg(boardId);
+        String unZipPath = request.getServletContext().getRealPath("/uploadFile/"); // 압축할 파일 저장할 위치
+        String unZipFile = checkReg.title; //압축할 파일 이름
+
+        //압축하기
+        CompressZip compressZip = new CompressZip();
+        try{
+            if (!compressZip.compress(path, unZipPath, unZipFile)){
+                return "fail";
+            }
+        } catch (Throwable e){
+            e.printStackTrace();
+        }
+
+        OutputStream out = null;
+        InputStream in = null;
+        String fileName = unZipFile+".zip";
+        // 실제 내보낼 파일명
+        String orgfilename = unZipFile+"파일.zip";
+        boolean skip = false;
+        String client = "";
+        File zipFile = null;
+
+        try{
+            // 파일을 읽어 스트림에 담기
+            try{
+                zipFile = new File(unZipPath, fileName);
+                in = new FileInputStream(zipFile);
+            }catch(FileNotFoundException fe){
+                skip = true;
+            }
+
+            client = request.getHeader("User-Agent");
+
+            // 파일 다운로드 헤더 지정
+            response.reset();
+            response.setContentType("application/octet-stream");
+            response.setHeader("Content-Description", "JSP Generated Data");
+            if(!skip){
+                // IE
+                if(client.indexOf("MSIE") != -1){
+                    response.setHeader ("Content-Disposition", "attachment;filename="+new String(orgfilename.getBytes("KSC5601"),"ISO8859_1"));
+                }else{
+                    // 한글 파일명 처리
+                    orgfilename = new String(orgfilename.getBytes("utf-8"),"iso-8859-1");
+                    response.setHeader("Content-Disposition", ("attachment; filename=\"" + orgfilename + "\""));
+                    response.setHeader("Content-Type", "application/octet-stream;charset=utf-8");
+                }
+
+                response.setHeader ("Content-Length", ""+zipFile.length());
+
+                out = response.getOutputStream();
+                byte b[] = new byte[(int)zipFile.length()];
+                int leng = 0;
+                while( (leng = in.read(b)) > 0 ){
+                    out.write(b,0,leng);
+                }
+            }
+            in.close();
+            out.close();
+        }catch(Exception e){
+            e.printStackTrace();
+            return "fail";
+        }
+        return "success";
+    }
+}
