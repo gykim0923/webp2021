2fd2899ef8ce5fcbef49676d77c60069ecb23102
gabrielyoon7 <gabrielyoon7@gmail.com>
2021-08-12 23:04:17 +0900

    [윤주현] 반응형에 따라 aside가 자동으로 들어가고 자동으로 나올 수 있도록 개선 및 기존 틀 유지하려는 버그 수정

--- web/assets/js/main.js
+++ web/assets/js/main.js
@@ -37,21 +37,21 @@ function j(t, e, o, i) {
 //     })
 // }
 //
-// window.addEventListener('DOMContentLoaded', (event) => {
-//     var w = window.innerWidth;
-//     if(w < 1200) {
-//         document.getElementById('sidebar').classList.remove('active');
-//     }
-// });
-// window.addEventListener('resize', (event) => {
-//     var w = window.innerWidth;
-//     if(w < 1200) {
-//         document.getElementById('sidebar').classList.remove('active');
-//     }else{
-//         document.getElementById('sidebar').classList.add('active');
-//     }
-// });
-//
+window.addEventListener('DOMContentLoaded', (event) => {
+    var w = window.innerWidth;
+    if(w < 1200) {
+        closeNav();
+    }
+});
+window.addEventListener('resize', (event) => {
+    var w = window.innerWidth;
+    if(w < 1200) {
+        closeNav();
+    }else{
+        openNav();
+    }
+});
+
 // document.querySelector('.burger-btn').addEventListener('click', () => {
 //     document.getElementById('sidebar').classList.toggle('active');
 // })
