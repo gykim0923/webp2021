858ea80cf4b500c4e106b907015a640d567cedda
gabrielyoon7 <gabrielyoon7@gmail.com>
2021-08-20 16:42:04 +0900

    [윤주현] Contributing 초안 작성

--- db.sql
+++ db.sql
@@ -1,16 +1,16 @@
 DROP DATABASE if EXISTS swaig ;
 CREATE DATABASE swaig DEFAULT CHARACTER SET utf8mb4; 
 USE swaig ;
-CREATE TABLE Example (
+CREATE TABLE example (
  `oid` INT(11) NOT NULL AUTO_INCREMENT,
  `title` VARCHAR(100) DEFAULT NULL,
  `content` VARCHAR(100) DEFAULT NULL,
  `date` DATE DEFAULT NULL, PRIMARY KEY (`oid`)
 ) ;
-INSERT INTO Example(oid, title, content, `date`) VALUES(1,'title1','content1','2021-06-26');
-INSERT INTO Example(title, content, `date`) VALUES('title2','content2','2021-06-27');
-INSERT INTO Example(title, content, `date`) VALUES('title3','content3','2021-06-28');
-INSERT INTO Example(title, content, `date`) VALUES('title4','내용내용내용','2021-06-28');
+INSERT INTO example(oid, title, content, `date`) VALUES(1,'title1','content1','2021-06-26');
+INSERT INTO example(title, content, `date`) VALUES('title2','content2','2021-06-27');
+INSERT INTO example(title, content, `date`) VALUES('title3','content3','2021-06-28');
+INSERT INTO example(title, content, `date`) VALUES('title4','내용내용내용','2021-06-28');
 
 CREATE TABLE menu_tabs(
     `tab_id` INT(10) NOT NULL,
